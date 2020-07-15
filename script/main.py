7695c2dd42933e3178386f3482515a1bd5150fca
h-komatsu <mu.on.the.moon54078@gmail.com>
2020-07-15 16:45:28 +0900

    headless on Heroku test

--- script/main.py
+++ script/main.py
@@ -2,66 +2,58 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-#from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 url = "https://www.columbiasports.co.jp/items/col/CU0134/"
 
 # データ取得
-resp = requests.get(url)
+#resp = requests.get(url)
 #print(resp.text)
 
-soup = BeautifulSoup(resp.content, "html.parser")
+#soup = BeautifulSoup(resp.content, "html.parser")
 #print(soup.title)
-buttons = soup.find_all("button") 
+#buttons = soup.find_all("button") 
 #print(buttons)
-for button in buttons:
-    print(button.get_text())
+#for button in buttons:
+#    print(button.get_text())
 
-message = soup.select_one("button.nostock")
-print("button.nostock=")
-print(message)
+#message = soup.select_one("button.nostock")
+#print("button.nostock=")
+#print(message)
 
-b = soup.find('div', class_="button")
-print("div.button=")
-print(b)
-message = b
+#b = soup.find('div', class_="button")
+#print("div.button=")
+#print(b)
+#message = b
 
 print("========")
 
 # ローカルに保存しているChrome Driverを指定(※デプロイするときはコメントアウトする)
 #driver_path = '/usr/local/bin/chromedriver'
 
-# Heroku上のChrome Driverを指定(※デプロイするときはコメントを外す)
-driver_path = '/app/.chromedriver/bin/chromedriver'
-
-# Headless Chromeをあらゆる環境で起動させるオプション
-#options = Options()
-#options.add_argument('--disable-gpu');
-#options.add_argument('--disable-extensions');
-#options.add_argument('--proxy-server="direct://"');
-#options.add_argument('--proxy-bypass-list=*');
-#options.add_argument('--start-maximized');
-#options.add_argument('--headless');
 
 # クローラーの起動
-#driver = webdriver.Chrome(executable_path=driver_path, options=options)
+#browser = webdriver.Chrome(executable_path=driver_path, options=options)
 
 # http get
-#driver.get(url)
+#browser.get(url)
 
 # ソースコードを取得
 #html = driver.page_source
 
 # ブラウザを終了する
+#browser.close()
 #driver.quit()
 
 # HTMLをパースする
-#soup = BeautifulSoup(html, 'lxml') # または、'html.parser'
+#soup = BeautifulSoup(html, 'html.parser') # または、'lxml'
 
 # スクレイピングした《今日の日本の天気予報の要約》を変数に格納
 #message = soup.select_one('button.nostock').get_text()
 
+
+#print(message)
 
 
 # LINEに通知させる関数
@@ -80,14 +72,35 @@ if __name__ == '__main__':
         #browser = webdriver.Chrome()   # 普通のChromeを制御する場合
 
         # HEADLESSブラウザに接続
-        browser = webdriver.Remote(
-            command_executor='http://selenium-hub:4444/wd/hub',
-            desired_capabilities=DesiredCapabilities.CHROME)
+        # Docker setting
+#        browser = webdriver.Remote(
+#            command_executor='http://selenium-hub:4444/wd/hub',
+#            desired_capabilities=DesiredCapabilities.CHROME)
+        # Heroku settings
+        # Heroku上のChrome Driverを指定(※デプロイするときはコメントを外す)
+        driver_path = '/app/.chromedriver/bin/chromedriver'
+        
+        # Headless Chromeをあらゆる環境で起動させるオプション
+        options = Options()
+        #options.add_argument('--disable-gpu');
+        #options.add_argument('--disable-extensions');
+        #options.add_argument('--proxy-server="direct://"');
+        #options.add_argument('--proxy-bypass-list=*');
+        #options.add_argument('--start-maximized');
+        options.add_argument('--headless');
+
+        browser = webdriver.Chrome(
+            executable_path=driver_path,
+            options=options)
+
 
         # Googleで検索実行
-        #message = execSearch(browser)
         browser.get(url);
+        time.sleep(1)
+
+        # 検索結果取得
         html = browser.page_source
+
         # HTMLをパースする
         soup = BeautifulSoup(html, 'html.parser') # または、'lxml'
         
